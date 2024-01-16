c435d08204ee54cdab7f518296495b5e177f547f
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-01-17 03:27:55 +1100

    [learn] add deprecated prefix

--- __learn__/src/cdk/a11y/aria-describer/aria-describer.ts
+++ __learn__/src/cdk/a11y/aria-describer/aria-describer.ts
@@ -1,7 +1,11 @@
-import {DOCUMENT} from '@angular/common';
-import {Inject, Injectable, OnDestroy, APP_ID, inject} from '@angular/core';
-import {Platform} from '@angular/cdk/platform';
-import {addAriaReferencedId, getAriaReferenceIds, removeAriaReferencedId} from './aria-reference';
+import { DOCUMENT } from "@angular/common";
+import { Inject, Injectable, OnDestroy, APP_ID, inject } from "@angular/core";
+import { Platform } from "@angular/cdk/platform";
+import {
+  addAriaReferencedId,
+  getAriaReferenceIds,
+  removeAriaReferencedId,
+} from "./aria-reference";
 
 /**
  * Contract used to register message elements and track how many registrations have the same message plus the reference to the message element used for `aria-describedby`
@@ -10,3 +14,17 @@ export interface RegisteredMessage {
   messageElement: Element; // Element containing message
   referenceCount: number; // how many elements that reference this message through `aria-describedby`
 }
+
+/**
+ * Body container's ID - all messages are appended here
+ * @deprecated Remove, obsolete
+ * @breaking-change 14.0.0
+ */
+export const MESSAGES_CONTAINER_ID = "cdk-describedby-message-container";
+
+/**
+ * ID prefix for each created message element
+ * @deprecated change this to private variable
+ * @breaking-change 14.0.0
+ */
+export const CDK_DESCRIBEDBY_ID_PREFIX = "cdk-describedby-message";
